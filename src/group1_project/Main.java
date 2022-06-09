113223c3cf47b75e3f3016e6b81f77ccbfe5e1a1
f8fbearcat <emden@knou.ac.kr>
2022-06-09 20:51:54 +0900

    initial commit

---
+++ src/group1_project/Main.java
@@ -0,0 +1,22 @@
+package group1_project;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		while(true){
+			System.out.println("1. 회원관리 이동");
+			System.out.println("2. 이벤트 관리 이동");
+			Scanner input = new Scanner(System.in);
+			int num = input.nextInt();
+			switch(num) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+			}
+		}
+	}
+}
